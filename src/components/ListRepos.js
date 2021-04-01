abaf987ccdcdb63fb76447f4b15a026fa08abfaf
vicyarmots <victor.yarmots@yandex.ru>
2021-04-01 16:02:45 +0300

    update repo link

--- src/components/ListRepos.js
+++ src/components/ListRepos.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { getRepo, getLoading } from '../Redux/repos/selectors';
+import '../style.css';
 
 function ListRepos() {
     const repos = useSelector(getRepo);
@@ -9,7 +10,11 @@ function ListRepos() {
         <div className="list">
             {isLoading === false ? (
                 repos.map((item, index) => {
-                    return <ul key={index}>{item.name}</ul>;
+                    return (
+                        <ul key={index}>
+                            <a href={item.html_url}>{item.name}</a>
+                        </ul>
+                    );
                 })
             ) : (
                 <h3>Loading...</h3>
