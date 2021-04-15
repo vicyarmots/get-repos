0dfba3b3799da538650a90b89cff580bb0ee83f0
vicyarmots <victor.yarmots@yandex.ru>
2021-04-15 03:02:05 +0300

    modified build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Form from './components/Form';
-
 import './style.css';
 
 function App() {
-	return (
-		<div className="App">
-			<Form />
-		</div>
-	);
+    return (
+        <div className="App">
+            <Form />
+        </div>
+    );
 }
 
 export default App;
