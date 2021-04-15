577834b868f1d9a20e1179fb6e9d4693554cb104
vicyarmots <victor.yarmots@yandex.ru>
2021-04-15 03:27:32 +0300

    modified store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/store/store';
+import store from '../src/redux/store/store';
 
 ReactDOM.render(
     <React.StrictMode>
